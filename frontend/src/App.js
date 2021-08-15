d1e3a5330650ec27f049e5cb8e48c332aae8b3b2
PatrickMac <patrickvalera500@gmail.com>
2021-08-15 11:51:01 -0700

    bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+      <Footer/>
     </div>
   );
 }
