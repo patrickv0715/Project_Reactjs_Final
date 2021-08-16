27339cfdca432c168bab83e2c7d1f0f55629e124
saish0 <patrickvalera50@yahoo.com>
2021-08-16 13:01:05 -0700

    convert to ESM(import) type

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const products = require('./data/products')
+// const express = require('express') Common js replaces with ESM
+import express from 'express'
+// const dotenv = require ('dotenv')
+import dotenv from 'dotenv'
+// const products = require('./data/products')
+import products from './data/products.js'
+
 const app = express()
 
-
+dotenv.config()
+const PORT=process.env.PORT || 5000
 app.get('/api/products',(req,res)=>{
     res.json(products)
 })
@@ -15,4 +21,4 @@ app.get('/api/products/:id',(req,res)=>{
 })
 
 
-app.listen(5000,console.log(`Server running on port 5000`))
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
