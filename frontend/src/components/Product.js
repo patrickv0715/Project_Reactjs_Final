a5fe351ce026fe897f562d55070aa68e4aab0844
saish0 <patrickvalera50@yahoo.com>
2021-08-21 00:26:37 -0700

    reducer and actions

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,10 +4,10 @@ import Rating from './Rating'
 import { Link } from 'react-router-dom'
 
 const Product = ({product}) => {
-    console.log(product._id)
+    console.log(`product ID: ${product._id} (${product.name.slice(0,5)}... from Product.js)`)
     return (
         <Card className='my-3 p-3 rounded'>
-            <Link to={`/product/${1}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top'/>
             </Link>
             <Card.Body>
