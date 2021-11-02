9f3b61d65dc8e7df11e745fa2f87ebf7b669e0d5
saish0 <patrickvalera50@yahoo.com>
2021-11-02 09:38:53 -0700

    payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,8 @@ import LoginScreen from "./screens/LoginScreen"
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 function App() {
   return (
     <Router>
@@ -23,6 +25,9 @@ function App() {
           <Route path='/cart/:id?' component={CartScreen}/> 
           <Route path='/profile' component={ProfileScreen}/>
           <Route path='/shipping' component={ShippingScreen}/>
+          <Route path='/payment' component={PaymentScreen}/>
+          <Route path='/placeorder' component={PlaceOrderScreen}/>
+
         </Container>
       </main>
       <Footer/>
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import {useDispatch,useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer'
 import { saveShippingAddress } from '../state/actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps'
+
 const ShippingScreen = ({history}) => {
     const {shippingAddress}=useSelector(state=>state.cart)
     const dispatch=useDispatch()
